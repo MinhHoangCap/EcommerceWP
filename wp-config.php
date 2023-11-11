56114837c9b1134ea63451b2e9c30441c5516fb5
MinhHoangCap <minhhoangcap@gmail.com>
2023-11-11 10:50:21 +0700

    Tao Wordpress Ecommerce

---
+++ wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/documentation/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'ecommerce' );
+
+/** Database username */
+define( 'DB_USER', 'root' );
+
+/** Database password */
+define( 'DB_PASSWORD', '' );
+
+/** Database hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '!.dF.u*kw(3x.>$BqL>V&eDP 8KstDcs<rsW>T/mflo>QzSY2d!n^m])}qBo:N~f' );
+define( 'SECURE_AUTH_KEY',  '?/$CIP:3x&L+ffT9zan[:Jbq+t0vD7ozhxf]11^dJ;Amx[2w(SViiO!OnS,6m4;:' );
+define( 'LOGGED_IN_KEY',    ')Y|o~Y7-G=#`Q7}FKaul,pD9n, 93(CIYmf2;&MYf8GP%=zJDrZgS{8JZbl]x0W;' );
+define( 'NONCE_KEY',        ' bsAI3h3QeG)qgnVDA5lwao9+I<]Z> vcZ](_nH8!C[je::%lpEV04HVa[l@v TK' );
+define( 'AUTH_SALT',        '~03B!%kSbf-7S2?#Hy~|(2[Dt$Ej5 d3xG8zEN##U<9aQ&7Uj`1$3#J7hxG5@+g9' );
+define( 'SECURE_AUTH_SALT', 'TS!NyL;g=MR0-+R eZjS4nz;+/k4&P1CRR7t$>h+aBW M-r[(d[F*{E.haO#TN3}' );
+define( 'LOGGED_IN_SALT',   '0#)Q4s#t|dER1_Jx?:SrvQ]{j&E2P_ZH1Es`C6+7:z{$w{jaNC1s~B;L@>&v0FkH' );
+define( 'NONCE_SALT',       'bteYFlao,p&aaw]=,5K/lkR,7_{0kA^* ,<3ZaOlup|2n`~7-B@?h-*A(hYBk`xV' );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
---
+++ wp-content/plugins/login-register-using-jwt/classes/common/Methods/class-jwtregister.php
@@ -0,0 +1,251 @@
+<?php
+/**
+ * Settings Controller
+ *
+ * JWT Login Settings controller
+ *
+ * @category   JWT Register
+ * @package    MoJWT\Method
+ * @author     miniOrange <info@miniorange.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html MIT/Expat, see LICENSE.php
+ * @link       https://miniorange.com
+ */
+
+namespace MoJWT\Methods;
+
+use MoJWT\JWTUtils;
+use MoJWT\JWTHandler;
+/**
+ * JWT Register
+ *
+ * @category JWT Register
+ * @package  MoJWT\Method\JWTRegister
+ * @author   miniOrange <info@miniorange.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html MIT/Expat, see LICENSE.php
+ * @link     https://miniorange.com
+ */
+class JWTRegister {
+
+	/**
+	 * Method slug
+	 *
+	 * @var string
+	 */
+	private $method_slug;
+
+	/**
+	 * Client Secret
+	 *
+	 * @var string
+	 */
+	private $client_secret;
+
+	/**
+	 * Current jwt config
+	 *
+	 * @var array
+	 */
+	private $current_config;
+
+
+	/**
+	 * Jwt config
+	 *
+	 * @var array
+	 */
+	private $method_config;
+
+	/**
+	 * Sign algo
+	 *
+	 * @var string
+	 */
+	private $sign_algo;
+
+	/**
+	 * Allowed role
+	 *
+	 * @var string
+	 */
+	private $allow_role;
+
+	/**
+	 * Default role
+	 *
+	 * @var string
+	 */
+	private $default_role;
+
+	/**
+	 * API Key for JWT User Registeration.
+	 *
+	 * @var string $api_key
+	 */
+	private $api_key;
+
+	/**
+	 * Constructor
+	 *
+	 * @return void
+	 **/
+	public function __construct() {
+		global $mj_util;
+		$this->method_slug    = 'jwtregister';
+		$this->current_config = $mj_util->get_plugin_config( 'mo_jwt_config_settings' ) ? ( $mj_util->get_plugin_config( 'mo_jwt_config_settings' ) ) : false;
+		$this->method_config  = $this->current_config ? ( $this->current_config['jwtcreate'] ) : false;
+		$this->sign_algo      = $this->method_config ? $this->method_config['mo_jwt_sign_algo'] : 'HS256';
+		$this->client_secret  = $this->method_config ? $this->method_config['mo_jwt_secret'] : false;
+		$this->default_role   = $this->current_config ? $this->current_config['jwtregister']['mo_jwt_register_role'] : 'subscriber';
+		$this->allow_role     = $this->current_config ? $this->current_config['jwtregister']['mo_jwt_allow_role'] : false;
+		$this->api_key        = isset( $this->current_config['jwtregister']['mo_jwt_api_key'] ) ? $this->current_config['jwtregister']['mo_jwt_api_key'] : '';
+
+		add_action( 'wp_ajax_mo_jwt_generate_new_api_key', array( $this, 'mo_jwt_generate_new_api_key' ) );
+	}
+
+	/**
+	 * Function to generate API key for endpoint authentication.
+	 */
+	public function mo_jwt_generate_new_api_key() {
+		global $mj_util;
+
+		$new_api_key = $mj_util->gen_rand_str( 32 );
+		$this->current_config['jwtregister']['mo_jwt_api_key'] = $new_api_key;
+		$mj_util->update_plugin_config( $this->current_config, 'mo_jwt_config_settings' );
+
+		$response = array(
+			'status'      => 'success',
+			'new_api_key' => $new_api_key,
+		);
+		wp_send_json( $response );
+	}
+
+	/**
+	 * Create user with jwt
+	 *
+	 * @param mixed $request Request to create user with jwt.
+	 *
+	 * @return string
+	 */
+	public function create_user_with_jwt( $request ) {
+
+		global $mj_util;
+
+		if ( isset( $request['username'] ) && isset( $request['apikey'] ) ) {
+
+			$api_key        = $request['apikey'];
+			$config         = $mj_util->get_plugin_config( 'mo_jwt_config_settings' );
+			$plugin_api_key = $config['jwtregister']['mo_jwt_api_key'];
+
+			if ( ! ( $api_key === $plugin_api_key ) ) {
+				$response = array(
+					'status'            => 'error',
+					'error'             => 'UNAUTHORIZED',
+					'code'              => '401',
+					'error_description' => 'Invalid API Key.',
+				);
+				$mj_util->send_json_response( $response );
+			}
+
+			$username = sanitize_text_field( $request['username'] );
+			$password = isset( $request['password'] ) ? sanitize_text_field( $request['password'] ) : $mj_util->gen_rand_str( 10 );
+
+			if ( empty( $password ) ) {
+				$response = array(
+					'status'            => 'error',
+					'error'             => 'BAD_REQUEST',
+					'code'              => '401',
+					'error_description' => 'User password cannot be empty.',
+				);
+				$mj_util->send_json_response( $response );
+			}
+
+			$user = wp_create_user( $username, $password );
+
+			if ( is_wp_error( $user ) ) {
+				$response = array(
+					'status'            => 'error',
+					'error'             => 'BAD_REQUEST',
+					'code'              => '401',
+					'error_description' => $user->get_error_message(),
+				);
+				$mj_util->send_json_response( $response );
+			}
+
+			$allow_default_role = 0;
+
+			if ( $this->allow_role ) {
+				$user_role = isset( $request['role'] ) ? sanitize_text_field( $request['role'] ) : false;
+				$user_role = str_replace( ' ', '_', $user_role );
+				$user_role = strtolower( $user_role );
+				if ( $user_role ) {
+					$all_roles = array_keys( wp_roles()->roles );
+					if ( ! in_array( $user_role, $all_roles, true ) ) {
+						$wp_user = new \WP_User( $user );
+						$wp_user->set_role( $this->default_role );
+						$response = array(
+							'status'            => 'error',
+							'error'             => 'BAD_REQUEST',
+							'code'              => '401',
+							'error_description' => 'The role passed in the request does not exists in the WordPress. Pass a correct role.',
+							'error_details'     => 'User created with default roles',
+						);
+						$mj_util->send_json_response( $response );
+					}
+
+					$wp_user = new \WP_User( $user );
+					$wp_user->set_role( $user_role );
+				} else {
+					$allow_default_role = 1;
+				}
+			}
+			if ( $allow_default_role ) {
+				$wp_user = new \WP_User( $user );
+				$wp_user->set_role( $this->default_role );
+			}
+
+			$client_secret = $this->client_secret;
+			$sign_algo     = $this->sign_algo;
+
+			if ( false === $client_secret || '' === $client_secret ) {
+				$response = array(
+					'status'            => 'error',
+					'error'             => 'BAD_REQUEST',
+					'code'              => '401',
+					'error_description' => 'Sorry, client secret is required to make a request. Contact to your administrator.',
+				);
+				$mj_util->send_json_response( $response );
+			}
+
+			$user = get_user_by( 'login', $username );
+
+			$token_data       = '';
+			$jwt_user         = array(
+				'sub'      => $user->ID,
+				'username' => $user->user_login,
+				'email'    => $user->user_email,
+			);
+			$jwt              = new JWTUtils();
+			$response         = $jwt->create_jwt_token( $jwt_user, $client_secret, $sign_algo, 60, $client_secret );
+			$response['code'] = 200;
+			return $response;
+
+		} elseif ( ! isset( $request['username'] ) ) {
+			$response = array(
+				'status'            => 'error',
+				'error'             => 'FORBIDDEN',
+				'code'              => '403',
+				'error_description' => 'Username is required.',
+			);
+			$mj_util->send_json_response( $response );
+		} else {
+			$response = array(
+				'status'            => 'error',
+				'error'             => 'FORBIDDEN',
+				'code'              => '403',
+				'error_description' => 'API Key is required.',
+			);
+			$mj_util->send_json_response( $response );
+		}
+	}
+
+}
